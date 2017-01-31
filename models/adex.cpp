0864f0efd62797a0279a85b2d59e4baf8803f6c9
Dan Meliza <dan@meliza.org>
2017-01-31 09:40:10 -0500

    working on refactoring interpolator

--- models/adex.cpp
+++ models/adex.cpp
@@ -25,13 +25,23 @@ T const * interpolate(double t, T const * data, double dt, size_t NC)
         return data + index * NC;
 }
 
+template <typename value_type, typename time_type>
+struct nn_interpolator {
+        typedef typename py::array_t<value_type> array_type;
 
-/** This observer does nothing. It's mostly here for benchmarking */
-template <typename Model>
-struct noop_observer {
-        typedef typename Model::state_type state_type;
-        py::array X;
-        void operator()(state_type const & x, double time) {}
+        nn_interpolator(array_type data, time_type dt)
+                : data(data), dt(dt) N(data.shape(0)) {}
+        value_type operator()(time_type t, size_t j) const {
+                return data.data(index(t), j);
+        }
+        size_t index(time_type t) const {
+                if (t < 0) return 0;
+                size_t i = std::round(t / dt);
+                return std::min(i, N - 1);
+        }
+        array_type data;
+        time_type dt;
+        size_t N;
 };
 
 template <typename Model>
@@ -57,34 +67,33 @@ struct pyarray_dense {
 
 namespace spyks {
 
-template <typename value_type, typename time_type=double>
+template <typename value_type, typename interpolator_type, typename time_type=double>
 struct adex {
         static const size_t N_PARAM = 10;
         static const size_t N_STATE = 2;
         static const size_t N_FORCING = 1;
         typedef typename std::array<value_type, N_STATE> state_type;
         value_type const * p;
-        value_type const * forcing;
-        time_type dt;
+        interpolator_type forcing;
 
-        adex (value_type const * p, value_type const * f, time_type forcing_dt)
-             : p(p), forcing(f), dt(forcing_dt) {}
+        adex (value_type const * p, interpolator_type f)
+                : p(p), forcing(f) {}
 
         void operator()(state_type const & X,
                         state_type & dXdt,
                         time_type t) const {
-                const double Iinj = interpolate(t, forcing, dt, N_FORCING)[0];
+                const double Iinj = forcing(t, 0);
                 const double x0 = -X[1];
-const double x1 = X[0] - p[2];
+                const double x1 = X[0] - p[2];
                 dXdt[0] = (Iinj + x0 - x1*p[1] + exp((X[0] - p[4])/p[3])*p[1]*p[3])/p[0];
-dXdt[1] = (x0 + x1*p[6])/p[5];
+                dXdt[1] = (x0 + x1*p[6])/p[5];
         }
 
         bool reset(state_type & X) const {
                 bool rp = X[0] >= p[9] ;
                 if (rp) {
                         X[0] = p[7];
-X[1] = X[1] + p[8];
+                        X[1] = X[1] + p[8];
                 }
                 return rp;
         }
@@ -136,46 +145,54 @@ using spyks::adex;
 PYBIND11_PLUGIN(adex) {
         typedef double value_type;
         typedef double time_type;
-        typedef adex<value_type, time_type> model;
+        typedef nn_interpolator<value_type, time_type> interpolator;
+        typedef adex<value_type, interpolator, time_type> model;
+
         py::module m("adex", "adaptive exponential integrate and fire model");
-        py::class_<model>(m, "model")
-                .def("__init__",
-                     [](model &m,
-                        py::array_t<value_type, py::array::c_style | py::array::forcecast> params,
-                        py::array_t<value_type, py::array::c_style | py::array::forcecast> forcing,
-                        time_type forcing_dt) {
-                             auto pptr = static_cast<value_type const *>(params.data());
-                             auto dptr = static_cast<value_type const *>(forcing.data());
-                             new (&m) model(pptr, dptr, forcing_dt);
-                     })
-                .def("__call__", [](model const & m, model::state_type const & X, time_type t) {
-                                model::state_type out;
-                                m(X, out, t);
-                                return out;
-                        })
-                .def("reset", [](model const & m, model::state_type & X) {
-                                bool r = m.reset(X);
-                                return std::make_pair(r, X);
-                        })
-                .def("clip", [](model const & m, model::state_type & X) {
-                                m.clip(X);
-                                return X;
+        py::class_<interpolator>(m, "interpolator")
+                .def(py::init<py::array, double>())
+                .def("__call__", [](interpolator const & d, time_type t, size_t j) {
+                                return d(t, j);
                         });
 
+        // py::class_<model>(m, "model")
+        //         .def("__init__",
+        //              [](model &m,
+        //                 py::array_t<value_type, py::array::c_style | py::array::forcecast> params,
+        //                 py::array_t<value_type, py::array::c_style | py::array::forcecast> forcing,
+        //                 time_type forcing_dt) {
+        //                      auto pptr = static_cast<value_type const *>(params.data());
+        //                      auto _forcing = interpolator(forcing, forcing_dt);
+        //                      new (&m) model(pptr, _forcing);
+        //              })
+        //         .def("__call__", [](model const & m, model::state_type const & X, time_type t) {
+        //                         model::state_type out;
+        //                         m(X, out, t);
+        //                         return out;
+        //                 })
+        //         .def("reset", [](model const & m, model::state_type & X) {
+        //                         bool r = m.reset(X);
+        //                         return std::make_pair(r, X);
+        //                 })
+        //         .def("clip", [](model const & m, model::state_type & X) {
+        //                         m.clip(X);
+        //                         return X;
+        //                 });
 
-        m.def("integrate", [](py::array_t<value_type, py::array::c_style | py::array::forcecast> params,
-                              model::state_type x0,
-                              py::array_t<value_type, py::array::c_style | py::array::forcecast> forcing,
-                              time_type forcing_dt, time_type stepping_dt) -> py::array {
-                      auto pptr = static_cast<value_type const *>(params.data());
-                      py::buffer_info forcing_info = forcing.request();
-                      auto dptr = static_cast<value_type const *>(forcing_info.ptr);
-                      time_type tmax = forcing_info.shape[0] * forcing_dt;
-                      model model(pptr, dptr, forcing_dt);
-                      return spyks::integrate(model, x0, tmax, stepping_dt);
-              },
-              "Integrates model from starting state x0 over the duration of the forcing timeseries",
-              "params"_a, "x0"_a, "forcing"_a, "forcing_dt"_a, "stepping_dt"_a);
-        m.def("integrate", &spyks::integrate<model>);
-        return m.ptr();
+
+        // m.def("integrate", [](py::array_t<value_type, py::array::c_style | py::array::forcecast> params,
+        //                       model::state_type x0,
+        //                       py::array_t<value_type, py::array::c_style | py::array::forcecast> forcing,
+        //                       time_type forcing_dt, time_type stepping_dt) -> py::array {
+        //               auto pptr = static_cast<value_type const *>(params.data());
+        //               py::buffer_info forcing_info = forcing.request();
+        //               auto dptr = static_cast<value_type const *>(forcing_info.ptr);
+        //               time_type tmax = forcing_info.shape[0] * forcing_dt;
+        //               model model(pptr, dptr, forcing_dt);
+        //               return spyks::integrate(model, x0, tmax, stepping_dt);
+        //       },
+        //       "Integrates model from starting state x0 over the duration of the forcing timeseries",
+        //       "params"_a, "x0"_a, "forcing"_a, "forcing_dt"_a, "stepping_dt"_a);
+        // m.def("integrate", &spyks::integrate<model>);
+        // return m.ptr();
 }
